da90a66a1683a21a218a40ff5fe97a0033d5f9a3
Matteo Valenzi <dilloawallace@hotmail.com>
2025-06-17 11:59:34 +0200

    Add project files.

---
+++ LabForWeb.EF/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LabForWeb.EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
